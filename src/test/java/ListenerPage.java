13f6d0fb5966471b51f2cc824ee8069d68b664d9
kalboussi karim <kalboussikarim3@gmail.com>
2025-01-03 22:23:28 +0100

    added an xml file that assigns listners to a test class

--- src/test/java/ListenerPage.java
+++ src/test/java/ListenerPage.java
@@ -5,27 +5,27 @@ import org.testng.ITestResult;
 public class ListenerPage implements ITestListener {
 
     public void onStart(ITestContext context) {
-
+        System.out.println("This is OnStart...");
     }
 
     public void onTestStart(ITestResult result) {
-
+        System.out.println("This is onTestStart...");
     }
 
     public void onTestSuccess(ITestResult result) {
-
+        System.out.println("This is onTestSuccess...");
     }
 
     public void onTestFailure(ITestResult result) {
-
+        System.out.println("onTestFailure");
     }
 
     public void onTestSkipped(ITestResult result) {
-
+        System.out.println("This is onTestSkipped...");
     }
 
     public void onFinish(ITestContext context) {
-
+        System.out.println("This is onFinish...");
     }
 
 
