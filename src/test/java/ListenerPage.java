823c49f931082c181cd9c86ace97b96abab5e885
kalboussi karim <kalboussikarim3@gmail.com>
2025-01-03 16:25:05 +0100

    added a Listner page that implements ITestListener

---
+++ src/test/java/ListenerPage.java
@@ -0,0 +1,32 @@
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class ListenerPage implements ITestListener {
+
+    public void onStart(ITestContext context) {
+
+    }
+
+    public void onTestStart(ITestResult result) {
+
+    }
+
+    public void onTestSuccess(ITestResult result) {
+
+    }
+
+    public void onTestFailure(ITestResult result) {
+
+    }
+
+    public void onTestSkipped(ITestResult result) {
+
+    }
+
+    public void onFinish(ITestContext context) {
+
+    }
+
+
+}
